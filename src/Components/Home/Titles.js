90d350807e089723c9a2197f47e64821aed90a09
asabogal <asabogal.git@gmail.com>
2019-02-21 16:44:22 -0500

    initial index.js stying + Titles css styles

--- src/Components/Home/Titles.js
+++ src/Components/Home/Titles.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './titles.css'
 
 const Title = () => {
   return (
     <div>
-      <h4>Alejandro Sabogal</h4>
-      <h1>FULL STACK DEVELOPER</h1>
-      <h5>JacaScript | React | Ruby | Rails</h5>
+      <h4 className="title-top">ALEJANDRO SABOGAL</h4>
+      <h1 className="title-center">FULL STACK DEVELOPER</h1>
+      <h5>JavaScript | React | Ruby | Rails</h5>
     </div>
   );
 };
