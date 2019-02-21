7aa3ea5c0d51e90b10ca54f980c059db1e1565d2
asabogal <asabogal.git@gmail.com>
2019-02-21 16:49:54 -0500

    add HomePage style

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../Components/Footer/Footer'
 import Titles from '../Components/Home/Titles'
+import './pages.css'
+import '../App.css';
 
 const HomePage= () => {
   return (
-    <div className="App">
+    <div className="App home-page">
       <Titles/> 
       <Footer/>
   </div>
