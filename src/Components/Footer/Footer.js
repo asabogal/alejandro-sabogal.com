c86e421be88d48059a2a23b1ea49d6e8f70f333c
asabogal <asabogal.git@gmail.com>
2019-02-22 00:07:25 -0500

    add styles remove css from footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './footer.css'
+import { footerDiv, footerIcon } from './styles'
 
 import email from './Icons/email.png'
 import github from './Icons/github.png'
@@ -8,18 +8,18 @@ import linkedin from './Icons/linkedin.png'
 const Footer = () => {
 
   return (
-    <div className="footer">
+    <div style={footerDiv}className="footer">
       
       <a href="mailto:asabogal.dev@gmail.com" target="_blank">
-      <img className="footer-icon" src={email}></img>
+      <img style={footerIcon} src={email}></img>
       </a>
 
       <a href="https://github.com/asabogal" target="_blank">
-      <img className="footer-icon" src={github}></img>
+      <img style={footerIcon} src={github}></img>
       </a>
       
       <a href="http://www.linkedin.com/in/alejandro-sabogal-dev" target="_blank">
-      <img className="footer-icon" src={linkedin}></img>
+      <img style={footerIcon} src={linkedin}></img>
       </a>
     </div>
   );
---
+++ src/Components/Footer/styles.js
@@ -0,0 +1,18 @@
+export const footerDiv = {
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  height: '100px',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  zIndex: 999,
+}
+
+export const footerIcon = {
+  margin: '0 10px',
+  height: '40px',
+  width: '40px',
+  padding: '1em',
+}
