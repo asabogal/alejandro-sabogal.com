996e6c4e55ec0a81851bf83abb6e1d3d5b67ff78
asabogal <asabogal.git@gmail.com>
2019-02-24 13:24:27 -0500

    initial about page

---
+++ src/Components/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import picture from './Images/Github Pic.jpg'
+
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <img src={picture}></img>
+      </div>
+
+      <div>
+        <p>Hi! I am Alejandro, a Software and Web Developer based in New York City.</p>
+        <p>I love coding just as much as I love making music. Both can be beautiful and complex, and I love creating this art. I am proficient in Ruby and JavaScript and enjoy builing efficient and useful applications. I have professional experience in other fileds; Finance, Hospitality, Entrepernuership. </p>
+      </div>   
+
+    </div>
+    
+   
+  );
+};
+
+export default About;
