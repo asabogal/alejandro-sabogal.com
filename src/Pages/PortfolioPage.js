09c42ba93d574b3d11e98a1bd3f4ff7894fe398a
asabogal <asabogal.git@gmail.com>
2019-02-25 17:24:56 -0500

    render PorfolioPage

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import NavHeader from '../Components/Navigation/NavHeader'
 import Footer from '../Components/Footer/Footer'
-import ProjectCard from '../Components/Projects/ProjectCard'
-
-import { projectData } from '../Data/projectData'
+import Portfolio from '../Containers/Portfolio'
 
 const PortfolioPage = () => {
 
- let sortedData = projectData.sort((a, b) => (a.id > b.id) ? -1 : 1);
-  console.log(sortedData)
-
-  const renderProjects = sortedData.map(project => {
-    return (
-      <ProjectCard key={project.id} project={project}/>
-    )
-  });
-
   return (
-    <div>
+    <div >
       <NavHeader/>
-      Portfolio Page...
-      {renderProjects}
+      <Portfolio/>
       <Footer/>
     </div>
+      
   );
 };
 
