cba5a02564a57ecdd0538ba976a7a8aac322842d
asabogal <asabogal.git@gmail.com>
2019-02-24 21:33:50 -0500

    initial PortfolioPage, Route and ProjectCard

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import NavHeader from '../Components/Navigation/NavHeader'
+import Footer from '../Components/Footer/Footer'
 import ProjectCard from '../Components/Projects/ProjectCard'
 
 const PortfolioPage = () => {
   return (
     <div>
+      <NavHeader/>
       Portfolio Page...
       <ProjectCard/>
+      <Footer/>
     </div>
   );
 };
