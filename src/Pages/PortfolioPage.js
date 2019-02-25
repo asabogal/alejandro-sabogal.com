264f3bf239883b5ccaa3a8e0b42903abdea9b0dd
asabogal <asabogal.git@gmail.com>
2019-02-24 21:51:18 -0500

    set initial data sorting and iteration for ProjectCard

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -3,12 +3,24 @@ import NavHeader from '../Components/Navigation/NavHeader'
 import Footer from '../Components/Footer/Footer'
 import ProjectCard from '../Components/Projects/ProjectCard'
 
+import { projectData } from '../Data/projectData'
+
 const PortfolioPage = () => {
+
+ let sortedData = projectData.sort((a, b) => (a.id > b.id) ? -1 : 1);
+  console.log(sortedData)
+
+  const renderProjects = sortedData.map(project => {
+    return (
+      <ProjectCard key={project.id} project={project}/>
+    )
+  });
+
   return (
     <div>
       <NavHeader/>
       Portfolio Page...
-      <ProjectCard/>
+      {renderProjects}
       <Footer/>
     </div>
   );
