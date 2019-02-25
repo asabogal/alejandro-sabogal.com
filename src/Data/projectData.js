9848757d57da934e94aa73ac6db5b8d029f9eac4
asabogal <asabogal.git@gmail.com>
2019-02-24 21:17:29 -0500

    add project data and project images

---
+++ src/Data/projectData.js
@@ -0,0 +1,43 @@
+import dj_charts from './projectImages/dj-charts.jpg'
+import recordbox_reviews from './projectImages/recordbox_reviews.jpg'
+import uefa_schedules from './projectImages/uefa_schedules.jpg'
+import interval_training_timer from './projectImages/interval_training_timer.jpg'
+
+export const projectData = [
+  {
+    id: 1,
+    name: "DJ Charts",
+    description: "Users (a.k.a DJ's or record collectors) can share their top vinyl record charts.",
+    tools: 'Sinatra, Ruby, HTML, CSS',
+    repoLink: "https://github.com/asabogal/dj-charts-sinatra-app",
+    image: dj_charts,
+    url: 'https://www.youtube.com/watch?v=nwtz3w8ToMg'
+  },
+  {
+    id: 2,
+    name: "Record Box Reviews",
+    description: "Record collection organizer and reviewer app, stores, shares and reviews user vinyl records.",
+    tools: 'Ruby on Rails, Bootstrap',
+    repoLink: "https://github.com/asabogal/recordbox-reviews",
+    image: recordbox_reviews,
+    url: 'https://www.youtube.com/watch?v=CHqRyEE8Jeg'
+  },
+  {
+    id: 3,
+    name: "UEFA Schedules",
+    description: "Get previous, future, and up to the minute European league soccer matches.",
+    tools: 'Javascript, JQuery, Ruby on Rails, Bootstrap',
+    repoLink: "https://github.com/asabogal/UEFA-schedules",
+    image: uefa_schedules,
+    url: 'https://www.youtube.com/watch?v=73hkA9pLtJg'
+  },
+  {
+    id: 4,
+    name: "Interval Training Timer",
+    description: "Customizable, High-Intensity-Interval-Training Timer.",
+    tools: 'React, Redux, Javascript, Ruby on Rails, React-strap',
+    repoLink: "https://github.com/asabogal/interval-training-timer",
+    image: interval_training_timer,
+    url: 'https://www.youtube.com/watch?v=vNy4p2KSsEI'
+  }
+]
