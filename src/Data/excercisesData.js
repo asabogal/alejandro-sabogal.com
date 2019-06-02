3e28f842d4892f71c413c6178213ba8ce50d47d7
asabogal <asabogal.git@gmail.com>
2019-06-02 19:25:42 -0400

    add excercises data and images

---
+++ src/Data/excercisesData.js
@@ -0,0 +1,13 @@
+import pig_game from './projectImages/pig-game.png'
+
+export const excerciseData = [
+  {
+    id: 1,
+    name: "JavaScrip Pig-Game",
+    description: "Dice Pig Game. An excercise in DOM Manipulation based on Jonas Schmedtmann's course.",
+    tools: "Vanilla JavaScript, HTML5, CSS3",
+    repoLink: "https://github.com/asabogal/pig-game",
+    image: pig_game,
+    url: "https://asabogal.github.io/pig-game/"
+  }
+]
