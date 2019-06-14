4863e49d3f9049f17c0ea1550faafb4a8da4f879
asabogal <asabogal.git@gmail.com>
2019-06-14 12:56:16 -0400

    add new exercise project and image

--- src/Data/exercisesData.js
+++ src/Data/exercisesData.js
@@ -1,13 +1,23 @@
 import pig_game from './projectImages/pig-game.png'
+import memory_game from './projectImages/memory_game.png'
 
 export const exercisesData = [
   {
     id: 1,
-    name: "JavaScript Pig-Game",
+    name: "Pig-Game",
     description: "Dice Pig Game. An excercise in DOM Manipulation based on Jonas Schmedtmann's course.",
     tools: "Vanilla JavaScript, HTML5, CSS3",
     repoLink: "https://github.com/asabogal/pig-game",
     image: pig_game,
     url: "https://asabogal.github.io/pig-game/"
+  },
+  {
+    id: 2,
+    name: "Memory Cards",
+    description: "Memory Card Game. An excercise in DOM Manipulation based on Marina Ferreira's tutorial.",
+    tools: "Vanilla JavaScript, HTML5, CSS3",
+    repoLink: "https://github.com/asabogal/memory-game",
+    image: memory_game,
+    url: "https://asabogal.github.io/memory-game/"
   }
 ]
