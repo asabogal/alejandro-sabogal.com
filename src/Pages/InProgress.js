28b838c02ac7dc509ae0d03c8925971f4d565061
asabogal <asabogal.git@gmail.com>
2019-12-05 19:31:30 -0500

    add client and api repo links

--- src/Pages/InProgress.js
+++ src/Pages/InProgress.js
@@ -5,6 +5,8 @@ const InProgress = () => {
   return (
     <div style={styles.div}>
       <h1 style={styles.h1}>IN PROGRESS</h1>
+      <a className="App-link" style={styles.link1} href='https://github.com/asabogal/cryptofolio-client'>Client</a>
+      <a className="App-link" style={styles.link2} href='https://github.com/asabogal/cryptofolio-api'>API</a>
     </div>
   );
 };
@@ -25,11 +27,26 @@ const styles = {
     h1: {
     position: 'absolute',
     top: '15%',
+    paddingLeft: '10px',
     color: 'grey',
     fontSize: '220px',
     lineHeight: '80%',
     zIndex: '0',
     opacity: '.4'
     // transform: 'rotate(-45deg)',
-  }
+  },
+    link1: {
+      position: 'absolute',
+      top: '85%',
+      left: '40%',
+      fontSize: '18px',
+      fontWeight: '400'
+  },
+  link2: {
+    position: 'absolute',
+    top: '85%',
+    left: '55%',
+    fontSize: '18px',
+    fontWeight: '400'
+}
 }
