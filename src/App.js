3f6fda87a5008249407b4d154d8298509d3b9b82
asabogal <asabogal.git@gmail.com>
2020-01-02 00:02:50 -0500

    pass NavHeader to Router

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import NavHeader from './Components/Navigation/NavHeader'
+import HomePage from './Pages/HomePage'
+import AboutPage from './Pages/AboutPage'
+import PortfolioPage from './Pages/PortfolioPage'
+import ResumePage from './Pages/ResumePage'
+import PageNotFound from './Pages/PageNotFound'
+import Helmet from 'react-helmet'
+import 'babel-polyfill'
+import Radium, { StyleRoot } from 'radium';
+
+const App = () => {
+
+  return (
+    <StyleRoot>
+      <Helmet>
+        <title>Alejandro Sabogal</title>
+        <meta name="description" content="My Personal Portfolio Site" />
+        <meta name="keywords" cpntent="alejandro, sabogal, alejandro sabogal, web developer, full stack developer, full-stack developer, full stack, full-stack, developer, portfolio" />
+      </Helmet>
+      <HashRouter basename='/'>
+        <NavHeader>
+          <Switch>
+            <Route exact path= '/' component={HomePage}/>
+            <Route exact path= '/about' component={AboutPage}/>
+            <Route exact path= '/portfolio' component={PortfolioPage}/>
+            <Route exact path= '/resume' component={ResumePage}/>
+            <Route component={PageNotFound}/>
+          </Switch>
+        </NavHeader>
+      </HashRouter>
+    </StyleRoot>
+ )
+}
+
+export default Radium(App);
