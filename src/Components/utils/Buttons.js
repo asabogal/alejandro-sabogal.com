9eb6002441f98b73b1b137350a881886c2f37400
asabogal <asabogal.git@gmail.com>
2020-01-02 01:57:36 -0500

    init with Burger button

---
+++ src/Components/utils/Buttons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export const BurgerButton = (props) => {
+  return (
+    <div style={{cursor: 'pointer'}}>
+      <FontAwesomeIcon 
+        icon={faBars} 
+        size={props.size} 
+        color={props.color}
+      />
+    </div>
+  );
+}
