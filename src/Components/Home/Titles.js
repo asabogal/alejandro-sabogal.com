5d0aa35d4dd69e2bfa8c9afd0965c108cefd199e
asabogal <asabogal.git@gmail.com>
2020-01-02 00:17:05 -0500

    change titles

--- src/Components/Home/Titles.js
+++ src/Components/Home/Titles.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { titleTop, titleCenter, animations } from './styles'
-import Radium from 'radium';
+import styled from 'styled-components';
 
 const Titles = () => {
 
   return (
-    <div>
-      <p style={[titleTop, animations.expandTitle]}>ALEJANDRO SABOGAL</p>
-      <div style={animations.fadeTitle}>
-        <h1 style={titleCenter}>FULL STACK<br></br>DEVELOPER</h1>
-        <h4>JavaScript | React | Ruby | Rails</h4>
-      </div>
-    </div>
+    <Container>
+     <h1>ALEJANDRO SABOGAL</h1>
+     <h2>SOFTWARE + WEB DEVELOPER</h2>
+     <h3>React | JavaScript | Node | Ruby | Rails</h3>
+    </Container>
   );
 };
 
-export default Radium(Titles);
+export default Titles;
+
+const Container = styled.div`
+  h1 {
+    font-size: 60px;
+    margin-block-start: 0px;
+    margin-block-end: 0px;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+  h2 {
+    font-size: 32px;
+    margin-block-start: 5px;
+    margin-block-end: 5px;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+`;
