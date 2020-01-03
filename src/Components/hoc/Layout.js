91aac9ea01bdc2bec57f3441a39b279ee4d62126
asabogal <asabogal.git@gmail.com>
2020-01-02 23:05:59 -0500

    add Layout

--- src/Components/hoc/Layout.js
+++ src/Components/hoc/Layout.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import NavHeader from '../navigation/NavHeader';
 import SideNavigation from '../navigation/SideNavigation';
+import {Backdrop} from '../shared_styled/SharedStyles';
 
 class Layout extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
-  }
+    this.state = { 
+      sideOpen: false
+     };
+  };
+
+  toggleSideNav = () => {
+    this.setState(prevState => {
+      return {sideOpen: !prevState.sideOpen};
+    })
+  };
+
+
   render() {
+    let sideNav;
+    let backdrop;
+
+    if (this.state.sideOpen) {
+      sideNav = <SideNavigation/>
+      backdrop = <Backdrop/>
+    };
+
     return (
       <div>
-        <NavHeader/>
-        <SideNavigation/>
+        <NavHeader toggleSideNav={this.toggleSideNav}/>
+        {sideNav}
+        {backdrop}
         {this.props.children}
       </div>
     );
-  }
-}
+  };
+};
 
 export default Layout;
