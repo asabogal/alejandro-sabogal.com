fcb4b0ed9f8f50a61b79e1b7812e69d0b95fe706
asabogal <asabogal.git@gmail.com>
2020-01-04 01:22:16 -0500

    add CloseButton

--- src/components/utils/Buttons.js
+++ src/components/utils/Buttons.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export const BurgerButton = (props) => {
   return (
-    <div style={{cursor: 'pointer'}}>
+    <BtnContainer>
       <FontAwesomeIcon 
         icon={faBars} 
         size={props.size} 
         color={props.color}
         onClick={props.click}
       />
-    </div>
+    </BtnContainer>
   );
-}
+};
+
+export const CloseButton = (props) => {
+  return (
+    <CloseBtn>
+      <FontAwesomeIcon 
+        icon={faTimes} 
+        size={props.size} 
+        color={props.color}
+        onClick={props.click}
+      />
+    </CloseBtn>
+  );
+};
+
+const BtnContainer = styled.div`
+  cursor: pointer;
+`;
+
+const CloseBtn = styled(BtnContainer)`
+  margin: 20px 20px 10px 0px;
+  :hover {
+    transform: scale(1.3)
+    }
+`;
