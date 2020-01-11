a45003566ba529196724f6c51c4b42b19a9885cf
asabogal <asabogal.git@gmail.com>
2020-01-11 00:30:11 -0500

    add Content container for cards

---
+++ src/components/about/Content.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import frontEnd from '../../Images/about_images/front_end.svg'
+import backEnd from '../../Images/about_images/back_end.svg'
+import ContentCard from './ContentCard';
+
+const Content = () => {
+
+  const cardContent = {
+    frontEnd: {
+      title: 'Front End Development',
+      paragraph: 'React, Hooks/Context, Redux, JavaScript, HTML5, CSS3, Responsive Design, Styled Components, Bootstrap, Material UI'
+    },
+    backEnd: {
+      title: 'Back End Architecture',
+      paragraph: 'Ruby, Ruby on Rails, Node, Express, SQL, Postgres, Google Cloud Platform, Google Firebase, Heroku'
+    }
+  }
+
+  return (
+    <ContentWrapper>
+      <h1>WHAT I DO</h1>
+      <CardsContainer>
+        <ContentCard
+          image={frontEnd}
+          title={cardContent.frontEnd.title}
+          paragraph={cardContent.frontEnd.paragraph}
+        />
+        <ContentCard
+          image={backEnd}
+          title={cardContent.backEnd.title}
+          paragraph={cardContent.backEnd.paragraph}/>
+      </CardsContainer>
+    </ContentWrapper>
+  );
+};
+
+export default Content;
+
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 25px auto;
+  background: #edf1f3;
+  h1 {
+    text-align: center;
+    color: #a0acb3;
+  }
+`;
+
+const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto;
+  grid-column-gap: 20px;
+  margin: 0px 50px 0px 50px;
+  @media (max-width: 500px) {
+    grid-template-columns: auto;
+  }
+`;
