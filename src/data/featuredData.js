ad7b8687d4661fbecd6e67181a22997f7b00dfba
asabogal <asabogal.git@gmail.com>
2020-01-16 12:39:14 -0500

    add Featured data

---
+++ src/data/featuredData.js
@@ -0,0 +1,28 @@
+import crypto1 from '../Images/featured_images/cryptofolio.png';
+import crypto2 from '../Images/featured_images/cryptofolio2.png';
+import recordbox from '../Images/featured_images/recordbox.png';
+import recordbox2 from '../Images/featured_images/cryptofolio2.png';
+
+export const featuredData = [
+  {
+    id: 1,
+    name: 'Cryptofolio Web App',
+    description: 'Cryptocurrency portfolio tracker. Track and monitor your favorite crypto coins',
+    stack: 'Full Stack Web Development | React, Ruby on Rails',
+    link: '/work/cryptofolio',
+    url: 'https://cryptofolio-web-app.herokuapp.com',
+    image1: crypto1,
+    image2: crypto2
+
+  },
+  {
+    id: 2,
+    name: 'Recordbox Reviews',
+    description: 'Share, review and organize your vinyl record collection',
+    stack: 'Full Stack Web Development | Ruby on Rails',
+    link: '/work/recordbox',
+    url :'https://recordbox-reviews.herokuapp.com',
+    image1: recordbox,
+    image2: recordbox2
+  }
+];
