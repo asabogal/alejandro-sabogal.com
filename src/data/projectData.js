09fc706555650de75753c7ca4a1c4137d34322bb
asabogal <asabogal.git@gmail.com>
2020-01-18 01:17:31 -0500

    update projectData

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,59 +1,62 @@
-import dj_charts from '../Images/projectImages/dj-charts.png'
-import recordbox_reviews from '../Images/projectImages/recordbox_reviews.png'
-import uefa_schedules from '../Images/projectImages/uefa_schedules.png'
-import interval_training_timer from '../Images/projectImages/interval_training_timer.png'
-import cryptofolio from '../Images/projectImages/cryptofolio_inprogress.png'
-
 export const projectData = [
-  
   {
     id: 1,
-    name: "DJ Charts",
-    description: "Users (a.k.a DJ's or record collectors) can share their top vinyl record charts.",
-    tools: 'Sinatra, Ruby, HTML, CSS',
-    repoLink: "https://github.com/asabogal/dj-charts-sinatra-app",
-    image: dj_charts,
+    year: 2018,
+    name: 'DJ Charts',
+    heading: 'Your favorite music records shared with the DJ Charts App',
+    description: 'Designed for DJs, this is CRUD (Create, Read, Update and Delete) web app helps authenticated users display and share their top 10 monthly charts. Visitors do not need to be logged in to view the chart and record database, however, a user must be logged in and authenticated in order to edit and/or delete his/her chart/records.',
+    stack: ['Sinatra', 'Ruby', 'HTML', 'CSS'],
+    tools: ['Sqlite3', 'ActiveRecord', 'Bcrypt'],
+    repoLink: 'https://github.com/asabogal/dj-charts-sinatra-app',
     url: 'https://www.youtube.com/watch?v=nwtz3w8ToMg',
-    video: 'https://www.youtube.com/watch?v=nwtz3w8ToMg'
-  },
-  {
-    id: 3,
-    name: "Record Box Reviews",
-    description: "Record collection organizer and reviewer app, stores, shares and reviews user vinyl records.",
-    tools: 'Ruby on Rails, Bootstrap, HTML, CSS, Google Cloud Platform',
-    repoLink: "https://github.com/asabogal/recordbox-reviews",
-    image: recordbox_reviews,
-    url: 'https://recordbox-reviews.herokuapp.com/',
-    video: 'https://youtu.be/CHqRyEE8Jeg'
+    path:'djcharts'
   },
   {
     id: 2,
-    name: "UEFA Schedules",
-    description: "Get previous, future, and up to the minute European soccer legue schedules.",
-    tools: 'Javascript, JQuery, Ruby on Rails, External & Rails API',
+    year: 2018,
+    name: 'UEFA-SCHEDULES',
+    heading: 'Follow your team and never miss a match with UEFA Schedules',
+    description: 'Designed with speed and practicality in mind, this API fetches previous, future, and up-to-the-minute European soccer league data from external sources, and serialized through Rails service objects. Data is rendered to users via JavaScript/AJAX.',
+    stack: ['JavaScript', 'Ruby on Rails API'],
+    tools: ['JQuery', 'Bootsrap','Potsman', 'Postgres', 'Faraday', 'Heroku'],
     repoLink: "https://github.com/asabogal/UEFA-schedules",
-    image: uefa_schedules,
     url: 'https://uefa-schedules.herokuapp.com/',
-    video: 'https://youtu.be/73hkA9pLtJg'
+    path: 'uefa-schedules'
+  },
+  {
+    id: 3,
+    year: 2018,
+    name: 'Recordbox Reviews',
+    heading: 'Music is to be shared! Oranize it, share it, discuss it with RECORDBOX REVIEWS',
+    description: 'Rails Full Stack application, uses MVC design and RESTful architectecture to help users keep track of their vinyl record collection.',
+    stack: ['Ruby on Rails'],
+    tools: ['Bootstrap', 'CCS Grid', 'Bcrypt', 'Facebook SDK', 'Google Cloud', 'Postgres', 'Heroku'],
+    repoLink: "https://github.com/asabogal/recordbox-reviews",
+    url: 'https://recordbox-reviews.herokuapp.com/',
+    path: 'recordbox-reviews'
   },
   {
     id: 4,
-    name: "Interval Training Timer",
-    description: "Customizable, High-Intensity-Interval-Training Timer.",
-    tools: 'React, Redux, Javascript, Ruby on Rails, React-strap',
+    year: 2019,
+    name: 'Interval Training Timer',
+    heading: 'Stay fit and be active with the INTERVAL TRAINING TIMER app',
+    description: 'Companion trainer with customizable workout database. User interaction with Redux state management.',
+    stack: ['React', 'Ruby on Rails API'],
+    tools: ['Redux', 'Redux Thunk', 'Reactstrap', 'Postgres', 'Heroku'],
     repoLink: "https://github.com/asabogal/interval-training-timer",
-    image: interval_training_timer,
     url: 'https://interval-training-timer.herokuapp.com/',
-    video: 'https://youtu.be/vNy4p2KSsEI'
+    path: 'interval-training-timer'
   },
   {
     id: 5,
-    name: "Cryptofolio",
-    description: "Cryptocurrency Portfolio Web App. Fetches live and historical crypto data.",
-    tools: 'React, Javascript, Styled Components, Ruby on Rails, RESTful API',
+    year: 2019,
+    name: 'Cryptofolio',
+    heading: 'Track your favorite crypto-coins with the Cryptofolio Web App',
+    description: 'User authenticated, cryptocurrency portfolio manager. Fetches real-time and historical data using async/await and back-end serializers. User friendly tools with Styled Components. State management through React Context API',
+    stack: ['React', 'JavaScript', 'Ruby on Rails API'],
+    tools: ['React Context', 'Styled Components', 'React High Charts,', 'Postgres', 'Bcrypt', 'CryptoCompare API', 'Heroku'],
     repoLink: 'https://github.com/asabogal/cryptofolio-production',
-    image: cryptofolio,
     url: 'https://cryptofolio-web-app.herokuapp.com/',
-    // video: 'https://youtu.be/vNy4p2KSsEI'
+    path: 'cryptofolio'
   }
 ]
