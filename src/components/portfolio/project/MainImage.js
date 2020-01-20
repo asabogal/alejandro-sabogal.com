9f1f4014940318da43e0e723acc296d6b55350bf
asabogal <asabogal.git@gmail.com>
2020-01-20 00:57:31 -0500

    init project MainImage

---
+++ src/components/portfolio/project/MainImage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainImage = (props) => {
+  return (
+    <ImageContainer>
+      <img src={props.image}/>
+    </ImageContainer>
+  );
+};
+
+export default MainImage;
+
+const ImageContainer = styled.div`
+  flex: 1 1;
+  img {
+    width: 55vw;
+    height: auto;
+    max-height: 100%;
+    max-width: 100%;
+    vertical-align: middle;
+    box-shadow: 10px 0px 20px rgba(0,0,0,0.5);
+  }
+  @media (max-width: 900px) {
+    img {
+      width: 100%;
+      height: auto;
+      margin-top: 15px;
+    }
+  }
+  `;
