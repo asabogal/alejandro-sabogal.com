9199e7689b954328a4a54859ffd91dec491992f7
asabogal <asabogal.git@gmail.com>
2020-01-28 12:54:14 -0500

    init Devices

---
+++ src/components/portfolio/project/Devices.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Devices = (props) => {
+  return (
+    <Container>
+      <img src={props.image} alt='device-images'/>
+    </Container>
+  );
+};
+
+export default Devices;
+
+const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  height: auto;
+  margin: 50px 0px;
+  img {
+    width: 100%;
+    height: 400px;
+  }
+`;
