8d7405ea50adcac038e6c99cbc2d6325cb1f183e
asabogal <asabogal.git@gmail.com>
2020-01-30 15:37:15 -0500

    init ExercisesContainer

---
+++ src/components/portfolio/ExercisesContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import ExerciseCard from './ExerciseCard';
+import {exercisesData} from '../../data/exercisesData';
+import Fade from 'react-reveal/Fade';
+
+const ExercisesContainer = () => {
+
+  let sorted = exercisesData.sort((a, b) => (a.id > b.id) ? -1 : 1)
+  
+    const renderExercises = sorted.map(project => {
+      return (
+        <ExerciseCard key={project.id} name={project.name} tools={project.tools} url={project.url} image={project.image}/>
+      )
+    })
+
+  return (
+    <Container>
+      <Fade up>
+      {renderExercises}
+      </Fade>
+      
+    </Container>
+  );
+};
+
+export default ExercisesContainer;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 60%;
+  row-gap: 10em;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin-top: 10vh;
+  margin-bottom: 30vh;
+  @media (max-width: 450px) {
+    grid-template-columns: 80%;
+  }
+`;
