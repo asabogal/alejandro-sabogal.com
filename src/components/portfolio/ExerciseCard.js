49f5c49a3075dc465d725fbbbe6e679d7e384b66
asabogal <asabogal.git@gmail.com>
2020-01-30 15:36:57 -0500

    init ExerciseCard

---
+++ src/components/portfolio/ExerciseCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import {MainBtn} from '../utils/Buttons';
+
+const ExerciseCard = ({name, tools, url, image}) => {
+  return (
+    <Container>
+      <ImageContainer>
+        <img src={image} alt='project-image'/>
+      </ImageContainer>
+      <InfoContainer>
+        <h4>{name}</h4>
+        <p>{tools}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <MainBtn width='130px'>View Project</MainBtn>
+        </a>
+      </InfoContainer>
+    </Container>
+  );
+};
+
+export default ExerciseCard;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: auto auto;
+`;
+
+const ImageContainer = styled.div`
+  opacity: 0.9;
+  padding: 15px;
+  box-shadow: 10px 10px 80px 0px rgba(0,0,0,0.12);
+  background-color: inherit;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const InfoContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto auto;
+  h4 {
+    font-weight: 600;
+    text-transform: uppercase;
+    font-size: 16px;
+    letter-spacing: -.05rem;
+    margin-block-start: 10px;
+    margin-block-end: 0px;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+  p {
+    font-weight:  500;
+    color: #707b81;
+  }
+`;
+
+
