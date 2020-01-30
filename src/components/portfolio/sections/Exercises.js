d8375800a2dc40638a2b36a93baa42f35f91d957
asabogal <asabogal.git@gmail.com>
2020-01-30 15:41:09 -0500

    init sections with Exercises

---
+++ src/components/portfolio/sections/Exercises.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Title from '../Title';
+import ExercisesContainer from '../ExercisesContainer';
+
+const Exercises = () => {
+  return (
+    <div>
+      {/* <Title
+        title="Full Stack Development"
+        description="Web Development | Deployed"
+      /> */}
+      <ExercisesContainer/>
+    </div>
+  );
+};
+
+export default Exercises;
