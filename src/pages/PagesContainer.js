9b49dd4a13badc66e718c1939a48e4ce982dec34
asabogal <asabogal.git@gmail.com>
2020-01-30 15:58:31 -0500

    remove old pages

--- src/pages/PagesContainer.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const HomeContainer = styled.div`
-  height: 80vh;
-  display: grid;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-`;
