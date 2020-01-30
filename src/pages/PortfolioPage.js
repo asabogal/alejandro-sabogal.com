53af6da61baf6164d764ec19ebbb50a6393673e9
asabogal <asabogal.git@gmail.com>
2020-01-30 15:51:24 -0500

    init PortfolioPage

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,16 +1,64 @@
-import React from 'react';
-import Portfolio from '../containers/Portfolio'
-import Exercises from '../containers/Exercises'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Excercises from '../components/portfolio/sections/Exercises';
+import Title from '../components/portfolio/Title';
+import Headers from '../components/portfolio/Headers';
+import ToTop from '../components/utils/ToTop';
 
-const PortfolioPage = () => {
+class PortfolioPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
 
-  return (
-    <div >
-      <Portfolio/>
-      <Exercises/>
-    </div>
-      
-  );
-};
+  // Refresh projectPage from cache and scroll to top
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.location.pathname !== this.props.location.pathname) {
+      window.location.reload(false);
+    }
+  }
+
+  componentDidMount = () => {
+    window.scroll(0, 0)
+  }
+
+  // scroll to top arrow functionality
+  arrowClickHandler = () => {
+    window.scroll({top: 0, left: 0, behavior: 'smooth' })
+  };
+  
+  render() {
+    return (
+      <div>
+        <Container>
+        <Title
+          title="Full Stack Development"
+          description="Deployed, production ready, front and back end architected applications"
+          />
+        <Headers/>
+      </Container>
+      <Container>
+        <Title
+          title="Front End Exercises"
+          description="A collection of small web applications intended to hone fundamental and advanced front-end development skills"
+          />
+        <Excercises/>
+      </Container>
+      <ToTop
+          size='1x'
+          click={this.arrowClickHandler}
+        />
+      </div>
+    );
+  }
+}
 
 export default PortfolioPage;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  @media (max-width: 980px) {
+    grid-template-columns: auto;
+  }
+`;
