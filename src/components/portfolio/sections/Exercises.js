8ac085a5b77166fe355234134f7295792ea0db74
asabogal <asabogal.git@gmail.com>
2020-01-30 15:46:50 -0500

    remove unused code

--- src/components/portfolio/sections/Exercises.js
+++ src/components/portfolio/sections/Exercises.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Title from '../Title';
 import ExercisesContainer from '../ExercisesContainer';
 
 const Exercises = () => {
   return (
     <div>
-      {/* <Title
-        title="Full Stack Development"
-        description="Web Development | Deployed"
-      /> */}
       <ExercisesContainer/>
     </div>
   );
