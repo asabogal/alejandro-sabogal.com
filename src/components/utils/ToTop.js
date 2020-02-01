ee95544406fbe20637b1d8df7522199f06a8ace6
asabogal <asabogal.git@gmail.com>
2020-02-01 00:29:59 -0500

    pass useScrollPosition hook + better style

--- src/components/utils/ToTop.js
+++ src/components/utils/ToTop.js
@@ -3,20 +3,34 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import Fade from 'react-reveal/Fade';
+import useScrollPosition from '../../hooks/useScrollPosition';
 
 const ToTop = (props) => {
-  
+
+  const scrollPosition = useScrollPosition();
+
+  const template = (
+    <Fade right>
+      <ArrowContainer onClick={props.click}>
+        <FontAwesomeIcon
+          icon={faAngleUp}
+          size={props.size}
+          color='#324955'
+        />
+      </ArrowContainer>
+    </Fade>
+  );
+
+  const renderToTop = (position) => {
+    return position.currentPosition > 850 ?
+      template
+      :
+      null
+  }
+
   return (
     <Wrapper>
-      <Fade right>
-        <ArrowContainer onClick={props.click}>
-          <FontAwesomeIcon
-            icon={faAngleUp}
-            size={props.size}
-            color='#324955'
-          />
-        </ArrowContainer>
-      </Fade>
+      {renderToTop(scrollPosition)}
     </Wrapper>
   );
 };
@@ -28,11 +42,15 @@ const Wrapper = styled.div`
   position: fixed;
   right: 0;
   bottom: 10%;
+  cursor: pointer;
 
 `;
 
 const ArrowContainer = styled.div`
-  opacity: 0.9;
-  padding: 10px 15px;
+  opacity: 0.8;
+  padding: 15px 22px;
   background-color: lightgray;
+  @media (max-width: 420px) {
+    padding: 15px 15px;
+  }
 `;
