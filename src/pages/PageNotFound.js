c9daa08ef0bea50e03ea04368d1d14660747b021
asabogal <asabogal.git@gmail.com>
2020-02-03 12:14:55 -0500

    fix page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 import error from '../Images/error.png'
 import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
   return (
-    <div className="not-found"> 
+    <Wrapper>
+      <Container>
       <img src={error} alt='error'></img>
       <h1 className="message">Page not found!</h1>
       <p>Oops! You seem lost. You have found a broken link or entered a URL that doesn't exist on this site.</p>
       <Link to="/">Back to site</Link>
-    </div>
+      </Container>
+    </Wrapper>
   );
 };
 
 export default PageNotFound;
+
+const Wrapper = styled.div`
+  height: 100vh;
+`;
+  
+const Container = styled.div`
+  display: grid;
+  justify-items: center;
+  /* align-items: center; */
+  padding-top: 50px;
+  padding-left: 40px;
+  padding-right: 40px;
+  text-align: center;
+`;
