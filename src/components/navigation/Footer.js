c71e2210d30c66c3ade819e57956b9b2d9e7c50c
asabogal <asabogal.git@gmail.com>
2020-02-03 11:46:43 -0500

    add site footer

---
+++ src/components/navigation/Footer.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <Container>
+      <div>
+        <p>&copy; 2020 - Alejandro Sabogal</p>
+      </div>
+      <SocialFooter>
+        <a href="https://www.github.com/asabogal" target="_blank" rel="noopener noreferrer"> 
+         <p>Git Hub</p>
+        </a>
+        <a href="https://www.linkedin.com/in/alejandro-sabogal-dev" target="_blank" rel="noopener noreferrer">
+          <p>Linked In</p>
+        </a>  
+        <a href="mailto:asabogal.dev@gmail.com">
+          <p>Contact Me</p>
+        </a>  
+      </SocialFooter>
+    </Container>
+  );
+};
+
+export default Footer;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  padding: 0px 30px;
+  font-size: 13px;
+  @media (max-width: 520px) {
+    flex-direction: column;
+    padding-bottom: 30px;
+  }
+`;
+
+const SocialFooter = styled.div`
+  display: flex;
+  justify-items: space-around;
+  p {
+    padding-left: 10px;
+  }
+  a { 
+    text-decoration: none;
+    color: inherit;
+  }
+
+`;
